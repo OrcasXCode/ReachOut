2dbcb685ee1fc79a9e551da8da44ea370a3cb05b
OrcasXCode <om.sureja@lpu.in>
2025-02-05 03:06:05 +0530

    report FE added

--- frontend/app/business/viewbusinessprofile/page.tsx
+++ frontend/app/business/viewbusinessprofile/page.tsx
@@ -246,19 +246,6 @@ const UserProfile = () => {
         </div>
 
         {/* Media Gallery */}
-        {/* <div className="bg-white rounded-lg shadow-lg p-6">
-          <h2 className="text-xl font-semibold mb-4">Media Gallery</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {profileData.mediaGallery.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Gallery ${index + 1}`}
-                className="w-full h-48 object-cover rounded-lg hover:opacity-90 transition-opacity cursor-pointer"
-              />
-            ))}
-          </div>
-        </div> */}
         <Typography variant="h5" color="blue-gray">Media Gallery</Typography>
         <div className="relative overflow-hidden w-full h-full py-20">
             <Carousel slides={slideData} />
@@ -552,7 +539,63 @@ const UserProfile = () => {
                   </div>
               </div>
           </div>
-        </section>            
+        </section>
+
+        {/* Report  */}
+        {/* <div className=" py-20 px-4 font-[sans-serif]">
+            <div className="max-w-4xl w-full mx-auto text-center grid grid-cols-2  gap-4">
+               <div>
+                    <h2 className="text-gray-800 text-2xl md:text-3xl font-extrabold mb-6 leading-[45px]">Raise a Complaint</h2>
+                    <p className="text-base text-gray-600">If you've encountered issues with a business, such as poor service, misleading information, or unethical practices, you can report it here.Our team will review your complaint and take the necessary steps to ensure transparency and fair resolution.
+                    </p>
+               </div>
+
+                <div className="flex flex-col gap-2">
+                    <div className="mt-12 bg-white flex items-center sm:p-4 p-2 mx-auto rounded-2xl border border-gray-300">
+                        <div className="relative w-full">
+                            <textarea
+                            className="peer h-full min-h-[150px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                            placeholder=" "></textarea>
+                            <label
+                            className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                            Message
+                            </label>
+                        </div>
+                    </div>
+                    <button className="bg-red-700 hover:bg-red-800 text-white text-base font-semibold py-4 px-4 sm:px-8 rounded-full focus:outline-none">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div> */}
+        <div className="py-20 px-4 font-[sans-serif]">
+            <div className="max-w-4xl w-full mx-auto text-center grid grid-cols-1  md:grid-cols-2 gap-9">
+                <div>
+                    <h2 className="text-gray-800 text-2xl md:text-3xl font-extrabold mb-6 leading-[45px]">Raise a Complaint</h2>
+                    <p className="text-base text-gray-600">
+                        If you've encountered issues with a business, such as poor service, misleading information, or unethical practices, you can report it here. 
+                        Our team will review your complaint and take the necessary steps to ensure transparency and fair resolution.
+                    </p>
+                </div>
+
+                <div className="flex flex-col gap-6 w-full items-center justify-center">
+                    <div className="w-full">
+                        <div className="relative ">
+                            <textarea
+                            className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline  transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                            placeholder=" "></textarea>
+                            <label
+                            className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 placeholder:text-gray-400">
+                            Write your complaint here...
+                            </label>
+                        </div>
+                    </div>
+                    <button className="bg-red-700 hover:bg-red-800 text-white text-base font-semibold py-4 w-full rounded-full focus:outline-none">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   );
